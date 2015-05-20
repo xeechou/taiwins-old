b786203ffd882d35c0786f689243b7feded37115
xeechou <xeechou@gmail.com>
2015-05-20 20:20:48 +0800

    now we know how to switch vt

--- vt_set.c
+++ vt_set.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,32 +6,51 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <time.h>
+#include <poll.h>
 
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <sys/signalfd.h>
 
 
 #include <linux/vt.h>
 #include <linux/kd.h>
-#include <xf86drm.h>	/* for drmSetMaster and drmDropMaster */
+#include <xf86drm.h>
 
 #ifndef KDSKBMUTE
 #define KDSKBMUTE	0x4B51
 #endif
 
 static const char *TTY0 = "/dev/tty0";
+static int signal_recv = 0;
 
 typedef struct tw_launcher {
 	int tty;	//init to 0
 	int ttynr;
 	char ttyname[16];
+	int signalfd;
 
-	int kb_mode;
+	int kb_mode;	//This is mode that we store for recovering KD_TEXT
 } tw_launcher;
-static int
-setup_tty(tw_launcher *tw)
+
+static int quit(tw_launcher *tw)
+{
+	/* recover the the context that we hade before */
+	struct vt_mode mode = {0};
+
+	if (ioctl(tw->tty, KDSKBMUTE, 0) &&
+	    ioctl(tw->tty, KDSKBMODE, tw->kb_mode))
+		return -1;
+	if (ioctl(tw->tty, KDSETMODE, KD_TEXT))
+		return -1;
+	mode.mode = VT_AUTO;
+	if (ioctl(tw->tty, VT_SETMODE, &mode) < 0)
+		return -1;
+	return 0;
+}
+static int setup_tty(tw_launcher *tw)
 {
 	struct stat st;
 	struct vt_mode mode = {0};
@@ -66,84 +84,154 @@ setup_tty(tw_launcher *tw)
 
 	if (ioctl(tw->tty, KDSETMODE, KD_GRAPHICS))
 		return -1;
+	//lets check if it works if we unmute the keyboad
+	if (ioctl(tw->tty, KDSKBMUTE, 0) &&
+	    ioctl(tw->tty, KDSKBMODE, tw->kb_mode))
+		return -1;
 	mode.mode = VT_PROCESS;
 	mode.relsig = SIGUSR1;
 	mode.acqsig = SIGUSR2;
 	if (ioctl(tw->tty, VT_SETMODE, &mode) < 0)
 		return -1;
 
-	getchar();
-	if (ioctl(tw->tty, KDSKBMUTE, 0) &&
-	    ioctl(tw->tty, KDSKBMODE, tw->kb_mode))
-		return -1;
-	if (ioctl(tw->tty, KDSETMODE, KD_TEXT))
-		return -1;
-	mode.mode = VT_AUTO;
-	if (ioctl(tw->tty, VT_SETMODE, &mode) < 0)
-		return -1;
+	return 0;
+}
+
+static int setup_signals(tw_launcher *tw)
+{
+	int ret;
+	sigset_t mask;
+	struct sigaction sa;
+
+	memset(&sa, 0, sizeof(sa));
+	sa.sa_handler = SIG_DFL;
+	sa.sa_flags = SA_NOCLDSTOP | SA_RESTART;
+	ret = sigaction(SIGCHLD, &sa, NULL);
+
+	sigaddset(&mask, SIGINT);
+	sigaddset(&mask, SIGTERM);
+	sigaddset(&mask, SIGUSR1);
+	sigaddset(&mask, SIGUSR2);
+	ret = sigprocmask(SIG_BLOCK, &mask, NULL);
+	if (ret)
+		return -errno;
+
+	tw->signalfd = signalfd(-1, &mask, SFD_NONBLOCK | SFD_CLOEXEC);
+	if (tw->signalfd < 0)
+		return -errno;
 
 	return 0;
 }
 
-//static int
-//handle_signal(struct weston_launch *wl)
-//{
-//	struct signalfd_siginfo sig;
-//	int pid, status, ret;
-//
-//	if (read(wl->signalfd, &sig, sizeof sig) != sizeof sig) {
-//		error(0, errno, "reading signalfd failed");
-//		return -1;
-//	}
-//
-//	switch (sig.ssi_signo) {
-//	case SIGCHLD:
-//		pid = waitpid(-1, &status, 0);
-//		if (pid == wl->child) {
-//			wl->child = 0;
-//			if (WIFEXITED(status))
-//				ret = WEXITSTATUS(status);
-//			else if (WIFSIGNALED(status))
-//				/*
-//				 * If weston dies because of signal N, we
-//				 * return 10+N. This is distinct from
-//				 * weston-launch dying because of a signal
-//				 * (128+N).
-//				 */
-//				ret = 10 + WTERMSIG(status);
-//			else
-//				ret = 0;
-//			quit(wl, ret);
-//		}
-//		break;
-//	case SIGTERM:
-//	case SIGINT:
-//		if (wl->child)
-//			kill(wl->child, sig.ssi_signo);
-//		break;
-//	case SIGUSR1:
-//		send_reply(wl, WESTON_LAUNCHER_DEACTIVATE);
-//		close_input_fds(wl);
-//		drmDropMaster(wl->drm_fd);
-//		ioctl(wl->tty, VT_RELDISP, 1);
-//		break;
-//	case SIGUSR2:
-//		ioctl(wl->tty, VT_RELDISP, VT_ACKACQ);
-//		drmSetMaster(wl->drm_fd);
-//		send_reply(wl, WESTON_LAUNCHER_ACTIVATE);
-//		break;
-//	default:
-//		return -1;
-//	}
-//
-//	return 0;
-//}
+static int
+handle_signal(tw_launcher *tw, int signo)
+{
+	switch (signo) {
+	//case SIGCHLD:
+	//	pid = waitpid(-1, &status, 0);
+	//	if (pid == wl->child) {
+	//		wl->child = 0;
+	//		if (WIFEXITED(status))
+	//			ret = WEXITSTATUS(status);
+	//		else if (WIFSIGNALED(status))
+	//			/*
+	//			 * If weston dies because of signal N, we
+	//			 * return 10+N. This is distinct from
+	//			 * weston-launch dying because of a signal
+	//			 * (128+N).
+	//			 */
+	//			ret = 10 + WTERMSIG(status);
+	//		else
+	//			ret = 0;
+	//		quit(wl, ret);
+	//	}
+	//	break;
+	//case SIGTERM:
+	case SIGINT:
+		return -1;
+		break;
+	case SIGUSR1:
+		signal_recv += 1;
+		//close_input_fds(wl);
+		//drmDropMaster(tw->tty);
+		ioctl(tw->tty, VT_RELDISP, 1);
+		printf("sdaf\n");
+		break;
+	case SIGUSR2:
+		signal_recv += 1;
+		ioctl(tw->tty, VT_RELDISP, VT_ACKACQ);
+		printf("sdaf\n");
+		//drmSetMaster(tw->tty);
+		//send_reply(wl, WESTON_LAUNCHER_ACTIVATE);
+		break;
+	default:
+		return -1;
+	}
+
+	return 0;
+}
+
+
 
 int main()
 {
 	tw_launcher tw;
 	memset(&tw, 0, sizeof(tw));
 	int ret = setup_tty(&tw);
+	if (ret) {
+		printf("error.\n");
+		return 0;
+	}
+	//ret = setup_signals(&tw);
+	//if (ret) {
+	//	quit(&tw);
+	//	printf("error.\n");
+	//	return 0;
+	//}
+
+	struct signalfd_siginfo fds;
+	sigset_t sigmask;
+	ssize_t s;
+
+	sigaddset(&sigmask, SIGUSR1);
+	sigaddset(&sigmask, SIGUSR2);
+	if (sigprocmask(SIG_BLOCK, &sigmask, NULL) == -1) {
+		quit(&tw);
+		return 0;
+	}
+	tw.signalfd = signalfd(-1, &sigmask, 0);
+	if (tw.signalfd == -1) {
+		quit(&tw);
+		printf("error in get signalfd\n");
+	}
+
+	struct pollfd pfd;
+	pfd.fd = tw.signalfd;
+	pfd.events = POLLIN;
+
+	int i;
+	for (;;) {
+		i = poll(&pfd, 1, -1);
+		if (i < 0)
+			return -1;
+		if (pfd.revents & POLLIN) {
+			s = read(tw.signalfd, &fds,
+				 sizeof(struct signalfd_siginfo));
+			if (s != sizeof(struct signalfd_siginfo)) {
+				quit(&tw);
+				return -1;
+			}
+			ret = handle_signal(&tw, fds.ssi_signo);
+		}
+		//sprintf(some, "%d\t", signal_recv);
+		//int log_fd = open("/tmp/tw_log", O_APPEND);
+		//write(log_fd, some, 2);
+		//close(log_fd);
+		//if (ret)
+		//quit(&tw);
+	}
+	ret = quit(&tw);
+	printf("%d %d %d\n", STDIN_FILENO, tw.signalfd, signal_recv);
 	if (ret)
 		printf("error.\n");
 }
